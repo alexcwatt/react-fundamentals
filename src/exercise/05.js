f260a9fe3de8e1e1d821dd8c895dea7ea740a817
Alex Watt <alex@alexcwatt.com>
2021-12-23 09:23:45 -0500

    Update exercise 5 - add a size prop, and match ...otherProps

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,15 +4,13 @@
 import * as React from 'react'
 import '../box-styles.css'
 
-function Box({className, style, children}) {
-  return <div className={`box ${className}`} style={{fontStyle: 'italic', ...style}}>
-    {children}
-  </div>
+function Box({size, style, ...otherProps}) {
+  return <div className={`box box--${size}`} style={{fontStyle: 'italic', ...style}} {...otherProps}></div>
 }
 
-const smallBox = <Box className="box--small" style={{backgroundColor: "lightblue"}}>small lightblue box</Box>
-const mediumBox = <Box className="box--medium"style={{backgroundColor: "pink"}}>medium pink box</Box>
-const largeBox = <Box className="box--large"style={{backgroundColor: "orange"}}>large orange box</Box>
+const smallBox = <Box size="small" style={{backgroundColor: "lightblue"}}>small lightblue box</Box>
+const mediumBox = <Box size="medium"style={{backgroundColor: "pink"}}>medium pink box</Box>
+const largeBox = <Box size="large"style={{backgroundColor: "orange"}}>large orange box</Box>
 
 function App() {
   return (
